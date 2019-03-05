819e7f65e7c6823b4af7a9b2e7e4f90076923f59
Ekta <Shahekta51995@gmail.com>
2019-03-05 18:27:46 -0500

    initial commits

---
+++ FinWiz/login/Register.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FinWiz
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        FinWizService wizService = new FinWizService();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_cmp_reg_Click(object sender, EventArgs e)
+        {
+            string[] data = new string[13];
+            data[0] = txt_reg_name.Text;
+            data[1] = txt_reg_conno.Text;
+            data[2] = txt_reg_add.Text;
+            data[3] = txt_reg_email.Text;
+            data[4] = txt_reg_username.Text;
+            data[5] = txt_reg_password.Text;
+            data[6] = txt_reg_repassword.Text;
+            data[7] = txt_cmp_name.Text;
+            data[8] = txt_cmp_add.Text;
+            data[9] = txt_cmp_email.Text;
+            data[10] = txt_cmp_conno.Text;
+            data[11] = txt_cmp_url.Text;
+            data[12] = txt_cmp_regid.Text;
+
+            wizService.cmp_register(data);
+
+        }
+
+        protected void btn_cmp_cancel_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("index.aspx");
+        }
+    }
+}
---
+++ FinWiz/webservice/FinWizService.asmx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Script.Serialization;
+using System.Web.Script.Services;
+using System.IO;
+using System.Net;
+
+using System.Text;
+
+namespace FinWiz
+{
+    /// <summary>
+    /// Summary description for FinWizService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class FinWizService : System.Web.Services.WebService
+    {
+        public FinWizService()
+        {
+
+
+            //Uncomment the following line if using designed components 
+            //InitializeComponent(); 
+        }
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public string get_user_login(string username, string password)
+        {
+            string[] result = { "", "", "" };
+            try
+            {
+                SqlParameter[] param = new SqlParameter[2];
+                param[0] = new SqlParameter("@username", username);
+                param[1] = new SqlParameter("@password", password);
+
+                DataTable dt = Dataacess.GetDataTable("Fiz_sp_get_user_login", CommandType.StoredProcedure, param);
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    result[0] = DefaultVar.success;
+                    result[1] = dt.Rows[0]["username"].ToString();
+                }
+                else
+                {
+                    result[0] = "nodata";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                result[0] = DefaultVar.error;
+                result[1] = ex.ToString();
+            }
+
+            return new JavaScriptSerializer().Serialize(result);
+        }
+
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public string get_forget_account(string EmpEmail)
+        {
+            string[] res = { "", "" };
+            try
+            {
+                SqlParameter[] param = new SqlParameter[1];
+                param[0] = new SqlParameter("@email", EmpEmail);
+                
+                DataTable dt = Dataacess.GetDataTable("Fiz_sp_get_forget_account", CommandType.StoredProcedure, param);
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    res[0] = DefaultVar.success;
+                    res[1] = dt.Rows[0]["EmpEmail"].ToString();
+                }
+                else
+                {
+                    res[0] = "nodata";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                res[0] = DefaultVar.error;
+                res[1] = ex.ToString();
+            }
+
+            return new JavaScriptSerializer().Serialize(res);
+        }
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public string cmp_register(string[] data)
+        {
+            string[] result = { "", "", "" };
+            try
+            {
+                SqlParameter[] param = new SqlParameter[13];
+                param[0] = new SqlParameter("@reg_name", data[0]);
+                param[1] = new SqlParameter("@reg_conno", data[1]);
+                param[2] = new SqlParameter("@reg_add", data[2]);
+                param[3] = new SqlParameter("@reg_email", data[3]);
+                param[4] = new SqlParameter("@reg_username", data[4]);
+                param[5] = new SqlParameter("@reg_password", data[5]);
+                param[6] = new SqlParameter("@cmp_name", data[7]);
+                param[7] = new SqlParameter("@cmp_add", data[8]);
+                param[8] = new SqlParameter("@cmp_email", data[9]);
+                param[9] = new SqlParameter("@cmp_conno", data[10]);
+                param[10] = new SqlParameter("@cmp_url", data[11]);
+                param[11] = new SqlParameter("@cmp_reg_id", data[12]);
+                
+
+                DataTable dt = Dataacess.GetDataTable("cmp_register", CommandType.StoredProcedure, param);
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    result[0] = DefaultVar.success;
+                    result[1] = dt.Rows[0]["username"].ToString();
+                }
+                else
+                {
+                    result[0] = "nodata";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                result[0] = DefaultVar.error;
+                result[1] = ex.ToString();
+            }
+
+            return new JavaScriptSerializer().Serialize(result);
+        }
+
+    }
+}
