858bd8832f60ea328b8eae8011263bd01cb172c1
rinkpatel <riyank27@gmial.com>
2019-03-06 11:01:19 -0500

    rinkwork init

--- FinWiz/webservice/FinWizService.asmx.cs
+++ FinWiz/webservice/FinWizService.asmx.cs
@@ -18,7 +18,7 @@ namespace FinWiz
     /// <summary>
     /// Summary description for FinWizService
     /// </summary>
-    [WebService(Namespace = "http://tempuri.org/")]
+    [WebService(Namespace = "http://rinkpatel.in/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
